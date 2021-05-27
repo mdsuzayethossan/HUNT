fce3bdde952a29369855d753320b5394d109b353
unknown <you@example.com>
2021-05-27 14:09:07 +0600

    first common

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+    $(function () {
+            // banner slider
+    $('.banner_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows:true,
+        fade:true,
+        speed:1500,
+        prevArrow:'.left',
+        nextArrow:'.right',
+        autoplaySpeed: 2000,
+      });
+
+    });
